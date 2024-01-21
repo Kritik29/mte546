d060189f652f9ca72954d3a0ed47b9e04c663a71
Kritik29 <Kritik29@users.noreply.github.com>
2024-01-21 15:00:23 -0500

    q3

--- a1/a1.py
+++ a1/a1.py
@@ -1,9 +1,8 @@
 import numpy as np 
-import matplotlib.pyplot as plt 
 from icecream import ic
 from scipy.stats import norm
 
-################## Q1
+################## Q1 (maximum likelihood classifier)
 
 # surface roughness measurements
 x_apples = np.array([2.56, 3.01, 3.22, 3.44, 3.62, 3.03, 2.40, 
@@ -27,6 +26,7 @@ pdf_lemons = lambda x : norm.pdf(x, lemons_mean, lemons_std)
 
 obs_x = 3.4
 
+print("Q1")
 print(f"Likelihood of being an apple: {pdf_apples(obs_x)}\n")
 print(f"Likelihood of being a lemon: {pdf_lemons(obs_x)}\n")
 
@@ -39,8 +39,9 @@ def max_likelihood_classifier(x):
     print("\nMost likely a lemon\n")
 
 max_likelihood_classifier(obs_x)
+print("Q1 end\n")
 
-################ Q2
+################ Q2 (bayesian classifier)
 # Prior Knowledge: 2000 lemons and 1000 apples
 
 n_apples = 1000
@@ -67,4 +68,60 @@ def bayesian_classifier(x):
   else:
     print(f"\nMost likely a lemon, with P(Lemon|x=3.4) = {P_lemon_given_x}\n")
 
+print("Q2")
 bayesian_classifier(obs_x)
+print("Q2 end\n")
+
+################ Q3 (joint probability classifier)
+
+P_lemon_yellow = 0.75
+P_apple_yellow = 0.25
+P_lemon_red = 0.08
+P_apple_red = 0.92
+
+def joint_probability_classifier(x:float, colour:str):
+  likelihood_apple_given_x = pdf_apples(x)
+  likelihood_lemon_given_x = pdf_lemons(x)
+
+  if(colour == "yellow"):
+    likelihood_apple_given_colour = P_apple_yellow
+    likelihood_lemon_given_colour = P_lemon_yellow
+  elif(colour == "red"):
+    likelihood_apple_given_colour = P_apple_red
+    likelihood_lemon_given_colour = P_lemon_red
+  else:
+    print("\nplease enter either 'yellow' or 'red")
+  
+  P_apple_given_x_and_colour = P_apple*likelihood_apple_given_x*likelihood_apple_given_colour
+  P_lemon_given_x_and_colour = P_lemon*likelihood_lemon_given_x*likelihood_lemon_given_colour
+
+  print("Non-normalized Values:")
+  if(P_apple_given_x_and_colour > P_lemon_given_x_and_colour):
+    print(f"\nMost likely an apple, P = {P_apple_given_x_and_colour}\n")
+  elif(P_apple_given_x_and_colour == P_lemon_given_x_and_colour):
+    print("\nEqually likely to be apple or lemon")
+  else:
+    print(f"\nMost likely a lemon, P = {P_lemon_given_x_and_colour}\n")
+  
+  print(f"\nProbability of being an apple: {P_apple_given_x_and_colour}")
+  print(f"\nProbability of being a lemon: {P_lemon_given_x_and_colour}")
+
+  normalizing_constant = P_apple_given_x_and_colour + P_lemon_given_x_and_colour
+
+  P_joint_apple = P_apple_given_x_and_colour / normalizing_constant
+  P_joint_lemon = P_lemon_given_x_and_colour / normalizing_constant
+
+  print("\nNormalized Values:")
+  if(P_joint_apple > P_joint_lemon):
+    print(f"\nMost likely an apple, P = {P_joint_apple}\n")
+  elif(P_joint_apple == P_joint_lemon):
+    print("\nEqually likely to be apple or lemon")
+  else:
+    print(f"\nMost likely a lemon, P = {P_joint_lemon}\n")
+  
+  print(f"\nProbability of being an apple: {P_joint_apple}")
+  print(f"\nProbability of being a lemon: {P_joint_lemon}")
+
+print("Q3")
+joint_probability_classifier(obs_x, "yellow")
+print("Q3 end\n")
